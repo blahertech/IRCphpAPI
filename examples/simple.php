ffabd42c2cdc226712fef8675bd92b355ebc1b8f
Benjamin Jay Young <byoung@blahertech.com>
2012-03-16 19:54:00 -0400

    starting the project

---
+++ examples/simple.php
@@ -0,0 +1,11 @@
+<?php
+	/**
+	 * Just a simple test to show how the API works.
+	 * 
+	 * @author Benjamin Jay Young <blaher@blahertech.org>
+	 * @version 1.0
+	 */
+	include('../IRCphpAPI.php');
+
+	$objIRC=IRCphpAPI('irc.chatspike.net', 6667, '#blahertech');
+?>
