c0d4142621aa2b1507221527c0d707d90fe9412e
Benjamin Jay Young <byoung@blahertech.com>
2012-03-17 10:01:28 +0800

    trying to get it to connect

--- IRCphpAPI.php
+++ IRCphpAPI.php
@@ -7,29 +7,60 @@
 	 */
 	class IRCphpAPI
 	{
-		private $objConnection, $strServer, $intPort, $aryChannels, $strNick;
+		private $rscConnection, $strServer, $intPort, $aryChannels, $strNick;
 		
 		public function __construct
 		(
 			$strServer, $intPort=6667, $strChannel=false, $strNick=false
 		)
 		{
-			$this->connect($strServer, $intPort);
-			
-			if ($strChannel)
+			if ($this->connect($strServer, $intPort))
 			{
-				$this->join($strChannel);
+				if ($strNick)
+				{
+					$this->nick($strNick);
+				}
+
+				while (!feof($rscConnection))
+				{
+					$strBuffer=fgets($rscConnection, 1024);
+					if ($strBuffer && $strBuffer!='')
+					{
+						echo '[RECIVE] ',$strBuffer,"<br />\n";
+					}
+
+					if ($strChannel && strpos($strBuffer, 422))
+					{
+						$this->join($strChannel);
+					}
+					if (substr($strBuffer, 0, 6)=='PING :')
+					{
+						$this->send('PONG :'.substr($strBuffer, 6));
+					}
+					flush();
+				}
 			}
-			
-			if ($strNick)
-			{
-				$this->nick($strNick);
-			}
+		}
+		
+		private function send($strMessage)
+		{
+			echo '[SEND] ',$strMessage,"<br />\n";
+			@fwrite
+			(
+				$this->rscConnection, $strMessage."\n\r", strlen($strMessage)
+			);
 		}
 		
 		private function connect($strServer, $intPort=6667)
 		{
-			
+			$this->rscConnection=@fsockopen($strServer, $intPort);
+			var_dump($strServer);
+			var_dump($intPort);
+			if ($this->rscConnection)
+			{
+				$this->send('PASS NOPASS');
+			}
+			return $this->rscConnection;
 		}
 		
 		private function disconnect()
@@ -39,12 +70,20 @@
 		
 		public function join($strChannel)
 		{
+			$strChannel=strtolower($strChannel);
+			if (substr($strChannel, 0, 1)!='#')
+			{
+				$strChannel='#'.$strChannel;
+			}
 			
+			$this->send('JOIN '.$strChannel);
+			$this->aryChannels[$strChannel]=array();
 		}
 		
 		public function quit($strChannel)
 		{
-			
+			// TODO: send quit command
+			unset($this->aryChannels[$strChannel]);
 		}
 		
 		private function changeNick($strNick)
@@ -56,7 +95,8 @@
 		{
 			if ($strNick)
 			{
-				// TODO: Send change Nick command.
+				$this->send('NICK '.$strNick);
+				$this->send('USER '.$strNick.' USING PHP IRC');
 				$this->changeNick($strNick);
 			}
 			
